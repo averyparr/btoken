a68cc5447e59b6612ff1174d50522b4cb5a5ccc7
Avery Parr <averyparr@gmail.com>
2024-11-11 01:36:09 -0500

    Add rust library testing to test script

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,9 @@
+# Rust library testing
+# Note: the `btoken` library is really just Python bindings for 
+# the `bit_tree` library, so we don't test `btoken` directly.
+cargo test --workspace 
+
+# Python library testing
 python3 -m venv tmp_test_venv
 source tmp_test_venv/bin/activate
 pip install numpy tiktoken pytest maturin
